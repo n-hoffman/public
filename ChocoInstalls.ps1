dc412cdf0378dea6476fa786800c6776367ac907
n-hoffman <62401977+n-hoffman@users.noreply.github.com>
2024-03-08 14:30:47 -0600

    Add files via upload

---
+++ ChocoInstalls.ps1
@@ -0,0 +1,14 @@
+﻿#Image Template Artifacts Script Version 1
+
+If (!(Get-Item "c:\Artifacts")) {
+    New-Item -ItemType Directory "c:\Artifacts"
+    }
+Start-Transcript -Path c:\Artifacts\Script-Transcript.log -Force
+Write-Host "Installing Chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Start-Sleep 15
+Write-Host "Installing 7Zip..."
+choco install 7zip -y
+Write-Host "Installing Notepad++..."
+choco install notepadplusplus -y
+Stop-Transcript
